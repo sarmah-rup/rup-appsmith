b3a5a0da0630d349fde176bb67412e5a04aff5c6
brainboxdotspace <sarmah.rup@gmail.com>
2024-05-14 10:57:42 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,14 +26,14 @@ function DemoPage() {
         <h1 className="font-display text-3xl font-bold leading-tight xl:text-6xl text-center">
           <span>
             <span className="relative inline-block bg-gradient-to-br from-primary-light-500 to-primary-light-500" style={{ backgroundClip: 'text', WebkitTextFillColor: 'transparent', color: 'transparent' }}>
-            Someplace we need
+            Someplace to
               <div className="pointer-events-none absolute -left-[5px] top-1/2 h-full w-full -translate-y-1/2 overflow-hidden border-l-2 border-l-primary-light-500 opacity-0" style={{ opacity: 1 }}>
                 <div className="h-full w-full -translate-x-full" style={{ background: 'linear-gradient(90deg, rgba(116, 65, 246, 0.2) 54.94%, rgba(116, 65, 246, 0) 96.77%)', translate: 'none', rotate: 'none', scale: 'none', transform: 'translate(0px, 0px)' }}></div>
               </div>
             </span>
             <span></span>
             <br></br>
-            <span>to call home 🏠</span>
+            <span>call home 🏡</span>
           </span>
         </h1>
           <div className="font-body text-xs leading-normal md:text-sm lg:text-base text-center mt-3xs text-primary-dark-800/70 dark:text-primary-neutral-50/80 lg:mt-2xs 2xl:mt-xs max-w-[35em] lg:max-w-[40em] 2xl:max-w-[45em]">
